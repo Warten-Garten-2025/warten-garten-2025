798d5987277758b23563bcfe2993e2c010c2888a
neve99 <165163178+neve99@users.noreply.github.com>
2025-10-23 16:05:35 +0200

    unknown svelte config things

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,12 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		prerender: {
-			entries: ['*']
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      entries: ['*']
+    }
+  }
 };
 
 export default config;
